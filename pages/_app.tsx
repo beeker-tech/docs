c7373e24f8dd926e2f8ed16d701c2c8ab26f0859
Sébastien Vanson <sebastien@pamplem.fr>
2023-11-01 20:04:31 +0100

    BEEK-48: Set montserrat font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,11 @@
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
